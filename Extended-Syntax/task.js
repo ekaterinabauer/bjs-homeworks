c982b20ba137f7f029a7c87579e20832e04c3648
Ekaterina Bauer <bauer-katerina@mail.ru>
2019-06-27 12:30:43 +0300

    Задание по второй лекции

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict"
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -11,6 +11,15 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
+    let d = Math.pow(b, 2) - 4 * a * c;
+ if (d > 0) {
+   x = [((- b + Math.sqrt(d)) / 2 * a), ((- b - Math.sqrt(d)) / 2 * a)];
+ } else if (d = 0) {
+   x = [- b / 2 * a];
+ } else {
+   x = [];
+ }
+ console.log(x);
     // код для задачи №1 писать здесь
     //return x;
 }
@@ -23,6 +32,15 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
+   let today = new Date();
+let year = today.getFullYear();
+let age = year - dateOfBirthday;
+  if (age >= 18) {
+  result = (`Не желаете ли олд-фэшн, ${name}?`);
+  } else {
+  result = (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+  }
+  console.log(result);
     // код для задачи №2 писать здесь
     //console.log(result)
     //return result;
@@ -35,6 +53,16 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
+    let sum = 0;
+  for (let i = 0; i < 5; i++) {
+    sum += marks[i];
+  }
+    if (marks.length > 5) { 
+    averageMark = ('Слишком много оценок', marks.slice(0, 5), (sum / marks.slice(0, 5).length));
+  } else {
+    averageMark = (sum / marks.length);
+    }
+    console.log(averageMark);
     // код для задачи №3 писать здесь
     //return averageMark;
 }
